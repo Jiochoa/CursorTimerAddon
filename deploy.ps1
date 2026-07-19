c3227f976889babf63a8e64d21a9a64a4a9b8e1c
Juan Ochoa <jochoa105@gmail.com>
2026-07-19 10:19:19 -0600

    clean up

--- deploy.ps1
+++
@@ -1,8 +0,0 @@
-# Copies the addon into the local WoW install for in-game testing.
-# After running, type /reload in-game to pick up the changes.
-$source = Join-Path $PSScriptRoot "CursorTimer"
-$dest = "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\CursorTimer"
-
-New-Item -ItemType Directory -Force $dest | Out-Null
-Copy-Item "$source\*" $dest -Force
-Write-Host "Deployed CursorTimer to $dest"
